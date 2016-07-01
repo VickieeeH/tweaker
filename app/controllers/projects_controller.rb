efa44ad855ff3672aa7a478e0da35e2f22fcc6e3
buyifly <buyifly@live.com>
2016-07-01 13:58:26 +1200

    Finished project page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,4 +2,27 @@ class ProjectsController < ApplicationController
   def index
     @projects = Project.all
   end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    project = Project.new(project_params)
+    if project.save
+      redirect_to project_path(project.id)
+    else
+      render action: "new"
+    end
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title, :description)
+  end
 end
